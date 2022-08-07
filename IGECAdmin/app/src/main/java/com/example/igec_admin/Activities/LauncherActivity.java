c8356ab173c86bd30fbdd4e2fa6ab2e8386b8288
Badr-1 <badrmohamedashraf@gmail.com>
2022-08-07 14:26:24 +0200

    Added SplashScreen

--- IGECAdmin/app/src/main/java/com/example/igec_admin/Activities/LauncherActivity.java
+++ IGECAdmin/app/src/main/java/com/example/igec_admin/Activities/LauncherActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 
@@ -14,26 +15,33 @@ public class LauncherActivity extends Activity {
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     public static final String IGEC = "IGEC";
     public static final String ID = "ID";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SharedPreferences preferences = getSharedPreferences(IGEC, MODE_PRIVATE);
-        if(preferences.getString(ID,"").equals("")) {
-            db.collection("employees").whereEqualTo("admin", true).limit(1).get().addOnSuccessListener(docs -> {
-                if (docs.size() != 0) {
-                    Intent intent = new Intent(LauncherActivity.this, LoginActivity.class);
-                    startActivity(intent);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences preferences = getSharedPreferences(IGEC, MODE_PRIVATE);
+                if (preferences.getString(ID, "").equals("")) {
+                    db.collection("employees").whereEqualTo("admin", true).limit(1).get().addOnSuccessListener(docs -> {
+                        if (docs.size() != 0) {
+                            Intent intent = new Intent(LauncherActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Intent intent = new Intent(LauncherActivity.this, MainActivity_navComp.class);
+                            startActivity(intent);
+                        }
+                        finish();
+                    });
                 } else {
                     Intent intent = new Intent(LauncherActivity.this, MainActivity_navComp.class);
                     startActivity(intent);
+                    finish();
                 }
-                finish();
-            });
-        }else
-        {
-            Intent intent = new Intent(LauncherActivity.this, MainActivity_navComp.class);
-            startActivity(intent);
-        }
+            }
+
+        }, 2000);
 
 
     }

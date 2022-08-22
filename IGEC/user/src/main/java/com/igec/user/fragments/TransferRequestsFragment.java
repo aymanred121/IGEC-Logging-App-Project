dde5f80ac8fe95cda5f1299544a3b7cb54a446ce
ayman hassan <ayman20191700160@cis.asu.edu.eg>
2022-08-22 21:01:33 +0200

    Refactored TransferRequestsFragment

--- IGEC/user/src/main/java/com/igec/user/fragments/TransferRequestsFragment.java
+++ IGEC/user/src/main/java/com/igec/user/fragments/TransferRequestsFragment.java
@@ -6,7 +6,6 @@ import static com.igec.common.CONSTANTS.EMPLOYEE_OVERVIEW_REF;
 import static com.igec.common.CONSTANTS.PROJECT_COL;
 import static com.igec.common.CONSTANTS.TRANSFER_REQUESTS_COL;
 
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -23,6 +22,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.firestore.FieldValue;
 import com.igec.user.activities.DateInaccurate;
 import com.igec.user.adapters.TransferAdapter;
 import com.igec.common.firebase.Allowance;
@@ -35,9 +35,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.WriteBatch;
 import com.igec.user.databinding.FragmentTransferRequestsBinding;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -49,6 +48,7 @@ public class TransferRequestsFragment extends Fragment {
     private Employee manager;
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private ArrayList<TransferRequests> requests;
+    private String day,year,month;
     private WriteBatch batch = FirebaseFirestore.getInstance().batch();
 
     public static TransferRequestsFragment newInstance(Employee manager) {
@@ -117,6 +117,20 @@ public class TransferRequestsFragment extends Fragment {
             adapter.notifyDataSetChanged();
         });
     }
+    private void updateDate() {
+        Calendar calendar = Calendar.getInstance();
+        year = String.valueOf(calendar.get(Calendar.YEAR));
+        month = String.format("%02d", calendar.get(Calendar.MONTH) + 1);
+        day = String.format("%02d", calendar.get(Calendar.DAY_OF_MONTH));
+        if (Integer.parseInt(day) > 25) {
+            if (Integer.parseInt(month) + 1 == 13) {
+                month = "01";
+                year = String.format("%d", Integer.parseInt(year) + 1);
+            } else {
+                month = String.format("%02d", Integer.parseInt(month) + 1);
+            }
+        }
+    }
 
     private void updateEmployeeData(TransferRequests request) {
         batch.update(EMPLOYEE_COL.document(request.getEmployee().getId()), "projectID", request.getNewProjectId()
@@ -127,79 +141,64 @@ public class TransferRequestsFragment extends Fragment {
         empInfo.add(request.getEmployee().getLastName());
         empInfo.add(request.getEmployee().getTitle());
         empInfo.add(manager.getId());
-        empInfo.add(manager.getProjectID());
+        empInfo.add(request.getNewProjectId());
         empInfo.add("1"); // by default because already in project
         Map<String, Object> empInfoMap = new HashMap<>();
         empInfoMap.put(request.getEmployee().getId(), empInfo);
         batch.update(EMPLOYEE_OVERVIEW_REF, empInfoMap);
     }
 
-    private void updateOldProjectData(TransferRequests request) {
-        PROJECT_COL.document(request.getOldProjectId()).get().addOnSuccessListener((value) ->
-        {
-            if (!value.exists()) return;
-            Project projectTemp = value.toObject(Project.class);
-            for (EmployeeOverview e : projectTemp.getEmployees()) {
-                if (e.getId().equals(request.getEmployee().getId())) {
-                    projectTemp.getEmployees().remove(e);
-                    break;
-                }
+    private void updateProjectData(TransferRequests request) {
+        //remove emp from old project
+        batch.update(PROJECT_COL.document(request.getOldProjectId()),"employees", FieldValue.arrayRemove(request.getEmployee()));
+        //add emp to new project
+        request.getEmployee().setProjectId(request.getNewProjectId());
+        request.getEmployee().setManagerID(manager.getId());
+        batch.update(PROJECT_COL.document(request.getNewProjectId()),"employees", FieldValue.arrayUnion(request.getEmployee()));
 
-            }
-            PROJECT_COL.document(request.getOldProjectId()).update("employees", projectTemp.getEmployees());
-        });
-    }
-
-    private void updateNewProjectData(TransferRequests request) {
         PROJECT_COL.document(request.getNewProjectId()).get().addOnSuccessListener((value) ->
         {
             if (!value.exists()) return;
             Project projectTemp = value.toObject(Project.class);
-            request.getEmployee().setProjectId(request.getNewProjectId());
-            request.getEmployee().setManagerID(manager.getId());
-            projectTemp.getEmployees().add(request.getEmployee());
-            PROJECT_COL.document(request.getNewProjectId()).update("employees", projectTemp.getEmployees());
             updateAllowancesData(request, projectTemp.getAllowancesList());
 
         });
     }
 
     private void updateAllowancesData(TransferRequests request, ArrayList<Allowance> projectAllowances) {
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-        String currentDateAndTime = sdf.format(new Date());
-        String day = currentDateAndTime.substring(0, 2);
-        String month = currentDateAndTime.substring(3, 5);
-        String year = currentDateAndTime.substring(6, 10);
-        EMPLOYEE_GROSS_SALARY_COL.document(request.getEmployee().getId()).get().addOnSuccessListener(value -> {
+        updateDate();
+      EMPLOYEE_GROSS_SALARY_COL.document(request.getEmployee().getId()).get().addOnSuccessListener(value -> {
             if (!value.exists())
                 return;
-            EmployeesGrossSalary temp = value.toObject(EmployeesGrossSalary.class);
-            temp.getAllTypes().removeIf(x -> x.getProjectId().equals(request.getOldProjectId()));
-            temp.getAllTypes().addAll(projectAllowances);
-            EMPLOYEE_GROSS_SALARY_COL.document(request.getEmployee().getId()).update("allTypes", temp.getAllTypes());
+            EmployeesGrossSalary employeesGrossSalary1 = value.toObject(EmployeesGrossSalary.class);
+            employeesGrossSalary1.getAllTypes().removeIf(x -> x.getProjectId().trim().equals(request.getOldProjectId()));
+            employeesGrossSalary1.getAllTypes().addAll(projectAllowances);
+            batch.update(EMPLOYEE_GROSS_SALARY_COL.document(request.getEmployee().getId()),"allTypes", employeesGrossSalary1.getAllTypes());
+
             EMPLOYEE_GROSS_SALARY_COL.document(request.getEmployee().getId()).collection(year).document(month).get().addOnSuccessListener(doc -> {
-                if (!doc.exists())
+                if (!doc.exists()){
+                    batch.commit();
                     return;
+                }
                 EmployeesGrossSalary employeesGrossSalary = doc.toObject(EmployeesGrossSalary.class);
                 employeesGrossSalary.getBaseAllowances().removeIf(a -> a.getProjectId().trim().equals(request.getOldProjectId()));
                 employeesGrossSalary.getBaseAllowances().addAll(projectAllowances);
-                db.document(doc.getReference().getPath()).update("baseAllowances", employeesGrossSalary.getBaseAllowances());
+                batch.update(db.document(doc.getReference().getPath()),"baseAllowances", employeesGrossSalary.getBaseAllowances());
+                batch.commit();
             });
         });
-
 
     }
 
     private void updateRequestStatus(TransferRequests request, int status) {
         batch = FirebaseFirestore.getInstance().batch();
-        TRANSFER_REQUESTS_COL.document(request.getTransferId()).update("transferStatus", status).addOnSuccessListener(unused -> {
-            if (status == 1) {
-                updateEmployeeData(request);
-                updateOldProjectData(request);
-                updateNewProjectData(request);
-                batch.commit();
-            } else if (status == 0) {/*do nothing*/}
-        });
+        if(status==1){
+            batch.update(TRANSFER_REQUESTS_COL.document(request.getTransferId()),"transferStatus", status);
+            updateEmployeeData(request);
+            updateProjectData(request);
+        }
+
+
     }
 
     private final TransferAdapter.OnItemClickListener oclRequest = new TransferAdapter.OnItemClickListener() {
